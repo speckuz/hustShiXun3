f81cc326b2ec5d54e4f805a5aaed6b7f10c0b735
speckuz <3588534904@qq.com>
2020-09-05 17:50:53 +0800

    整合信息模块和兴趣模块并且支持查询用户兴趣

---
+++ backend/userpart/src/main/java/hust/sse/vini/userpart/user/User.java
@@ -0,0 +1,88 @@
+package hust.sse.vini.userpart.user;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+    @Column(length=32,unique = true,nullable = false)
+    private String userName;
+    @Column(nullable = false)
+    private String passwordHash;
+    private Boolean gender;
+    private byte[] thumbnail;
+    private Date birthday;
+    private String location;
+    @ElementCollection
+    private List<String> interests = new ArrayList<>();
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public byte[] getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(byte[] thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<String> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(List<String> interests) {
+        this.interests = interests;
+    }
+}
---
+++ backend/userpart/src/test/java/hust/sse/vini/userpart/UserpartApplicationTests.java
@@ -0,0 +1,13 @@
+package hust.sse.vini.userpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserpartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

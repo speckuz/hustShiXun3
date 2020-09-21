bdb6799b00b71e162dcb9cf5a67fd24436fbcff1
LTW <ltwsamuel@outlook.com>
2020-09-21 22:10:48 +0800

    Lengthened the expiry time.

--- backend/vini/src/main/java/sse/hust/vini/auth/TokenUtils.java
+++ backend/vini/src/main/java/sse/hust/vini/auth/TokenUtils.java
@@ -8,6 +8,6 @@ import java.util.Date;
 public class TokenUtils {
     public static String generateToken(Integer userId){
         Date date = new Date();
-        return "Bearer "+JWT.create().withExpiresAt(new Date(date.getTime()+300*1000L)).withAudience(String.valueOf(userId)).sign(Algorithm.HMAC256("vini_sns"));
+        return "Bearer "+JWT.create().withExpiresAt(new Date(date.getTime()+30*86400*1000L)).withAudience(String.valueOf(userId)).sign(Algorithm.HMAC256("vini_sns"));
     }
 }
